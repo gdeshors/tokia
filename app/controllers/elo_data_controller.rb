faedc9d1f49555c410f8303398ef8776b12e4ac9
Guillaume Deshors <guillaume@deshors.net>
2013-12-08 19:22:36 +0100

    correction bug graphe jusqu'à hier minuit

--- app/controllers/elo_data_controller.rb
+++ app/controllers/elo_data_controller.rb
@@ -2,7 +2,7 @@ class EloDataController < ApplicationController
   def history
     data = []
 
-    (Match.where("winner_id is not null").where(:created_at => (Date.today - 4)..Date.today)).each do |m|
+    (Match.where("winner_id is not null").where(:created_at => (Date.today - 4)..Date.tomorrow)).each do |m|
       data.push(date: m.created_at, m.ai_1_id => m.new_elo_1, m.ai_2_id => m.new_elo_2 )
     end
 
