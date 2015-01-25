7c00d2bb5aaf4a385cec5b266eb7be0502476d10
Guillaume Deshors <guillaume@deshors.net>
2015-01-25 21:18:04 +0100

    chgt php5 en php pour le serveur mageia... todo en conf !

--- app/helpers/ais_helper.rb
+++ app/helpers/ais_helper.rb
@@ -5,7 +5,7 @@ module AisHelper
     return case ai.language
       when 'java 1.6' then  "java -cp #{ai.filename} #{ai.firstparam}"
       when 'scala 2.10.3' then  "java -cp env/scala_2.10.3/*:#{ai.filename} #{ai.firstparam}"
-      when 'php 5' then  "php5 #{ai.filename} #{ai.firstparam}"
+      when 'php 5' then  "php #{ai.filename} #{ai.firstparam}"
     end
 
 
