deb6913969addb10a96f6051b0ff9ba384f6bc37
Guillaume Deshors <guillaume@deshors.net>
2015-02-03 22:39:50 +0100

    nom du fichier serveur

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -27,7 +27,7 @@ class StaticPagesController < ApplicationController
 
   def download
     case params[:id]
-      when '1' ; send_file("/home/tokserver/server/tok-server-1.0-SNAPSHOT.jar")
+      when '1' ; send_file("/home/tokserver/server/tok-server.jar")
       when '2' ; send_file("/home/tokserver/env/scala_2.10.3/scala-library-2.10.3.jar")
       when '3' ; send_file("/home/tokserver/env/scala_2.10.3/scala-actors-2.10.3.jar")
       when '4' ; send_file("/home/tokserver/env/scala_2.10.3/log4j-1.2.16.jar")
