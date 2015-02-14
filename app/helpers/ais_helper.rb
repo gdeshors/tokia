b06f21bf39e31b98264d6ab516aa48416af8e6b7
Guillaume Deshors <guillaume@deshors.net>
2015-02-14 20:07:28 +0100

    ajout de mono

--- app/helpers/ais_helper.rb
+++ app/helpers/ais_helper.rb
@@ -4,8 +4,9 @@ module AisHelper
   def command_line_for(ai)
     return case ai.language
       when 'java 1.6' then  "java -cp #{ai.filename} #{ai.firstparam}"
-      when 'scala 2.10.3' then  "java -cp env/scala_2.10.3/*:#{ai.filename} #{ai.firstparam}"
+      when 'scala 2.10.3' then  "java -cp /home/tokserver/env/scala_2.10.3/*:#{ai.filename} #{ai.firstparam}"
       when 'php 5' then  "php #{ai.filename} #{ai.firstparam}"
+      when 'mono' then "mono #{ai.filename} #{ai.firstparam}"
     end
 
 
